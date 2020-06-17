442dc3611eb94204c4fbf59e684b86ead2490475
Noel Espino Córdova <noelespinocordova@gmail.com>
2020-06-16 21:01:56 -0500

    Day 1

---
+++ Day 1/Day 1.playground/Contents.swift
@@ -0,0 +1,66 @@
+/*
+ * Documento creado a partir del reto 100 Days of Swift
+ *
+ * Día 1
+ * https://www.hackingwithswift.com/100/1
+ */
+
+import UIKit
+
+// Variables
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+// Strings and integers
+
+var name = "Noel Espino"
+
+var age = 25
+
+var population = 8_000_000
+
+// Multi-line strings
+
+var multilineStr1 = """
+This goes
+over multiple
+lines
+"""
+
+var multilineStr2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+
+var pi = 3.1415
+
+var awesome = true
+
+// String interpolation
+
+var score = 85
+
+var scoreResult = "Your score was \(score)"
+
+var addition = "Result of 2 + 2: \(2 + 2)"
+
+// Constants
+
+let taylor = "swift"
+
+// Type annotations
+
+let album: String = "Miss Anthropocene"
+
+let artist: String = "Grimes"
+
+let year: Int = 2020
+
+let rate: Float = 9.9
+
+let owned: Bool = true
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 100 Days of Swift
 
+Realizado en base al reto [100 Days of Swift](https://www.hackingwithswift.com/100)
+
+* Día 1: 16 de junio 2020
